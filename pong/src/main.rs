e4c44843539d1d53b91447eff919a812f43bd393
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-07-10 11:44:48 +0200

    Started setup function

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,7 +1,41 @@
-use bevy::prelude::*;
+use bevy::{prelude::*};
+
+const PADDLE_SPEED: f32 = 350.0;
+
+const BALL_SIZE: f32 = 50.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
+struct Ball;
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands
+        .spawn()    
+        .insert(Ball)
+        .insert_bundle( SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.9, 0.9, 0.9),
+                ..default()
+            },
+            transform: Transform { 
+                translation: Vec3::new(0.0, 0.0, 1.0), 
+                scale: Vec3::new(50.0, 50.0, 1.0),
+                ..default()
+            },
+        ..default()
+    });
 }
