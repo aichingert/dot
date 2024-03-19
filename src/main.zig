3b6e56916c8a3ea93a3a9f44668f717e31f515bd
aichingert <aichinger.tobias13@gmail.com>
2024-03-19 10:22:21 +0100

    feat: finished first minimum viable product

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,24 @@
 const std = @import("std");
 
-fn searchDir(ident: []const u8) !void {
-    std.debug.print("Dir: {s}\n", .{ident});
+const allocator = std.heap.page_allocator;
 
+fn containsTerm(line: []const u8, term: []const u8) ?usize {
+    var loc: usize = 0;
+    var end: usize = term.len;
+
+    while (end + 1 < line.len) {
+        if (std.mem.eql(u8, line[loc..end], term)) {
+            return loc;
+        }
+
+        loc += 1;
+        end += 1;
+    }
+
+    return null;
+}
+
+fn searchDir(ident: []const u8, term: []const u8) !void {
     var dir = try std.fs.cwd().openIterableDir(ident, .{});
     defer dir.close();
 
@@ -12,27 +28,27 @@ fn searchDir(ident: []const u8) !void {
         const path = try concatDir(ident, entry.name);
 
         if (entry.kind == .directory) {
-            try searchDir(path);
-        } else if (entry.kind == .file) {
-            var file = try std.fs.cwd().openFile(path, .{ .mode = std.fs.File.OpenMode.read_only });
+            try searchDir(path, term);
+            continue;
+        }
+        var file = try std.fs.cwd().openFile(path, .{ .mode = std.fs.File.OpenMode.read_only });
+        defer file.close();
 
-            var buf_reader = std.io.bufferedReader(file.reader());
-            var in_stream = buf_reader.reader();
-            var buf: [1024]u8 = undefined;
+        var buf_reader = std.io.bufferedReader(file.reader());
+        var in_stream = buf_reader.reader();
+        var buf: [1024]u8 = undefined;
 
-            std.debug.print("    file: {s} | {any}\n", .{ entry.name, entry.kind });
-            while (in_stream.readUntilDelimiterOrEof(&buf, '\n') catch {
-                continue;
-            }) |line| {
-                _ = line;
+        while (in_stream.readUntilDelimiterOrEof(&buf, '\n') catch {
+            continue;
+        }) |line| {
+            if (containsTerm(line, term)) |loc| {
+                std.debug.print("Dir: {s}\n", .{ident});
+                std.debug.print("   - file: {s} | {any}\n", .{ entry.name, entry.kind });
+                std.debug.print("       - {d}\n", .{loc});
             }
-
-            file.close();
         }
     }
 }
-
-const allocator = std.heap.page_allocator;
 
 fn concatDir(f: []const u8, s: []const u8) ![]const u8 {
     var res = try allocator.alloc(u8, f.len + s.len + 1);
@@ -46,10 +62,10 @@ pub fn main() !void {
     var args = std.process.args();
     _ = args.skip();
 
-    _ = args.next() orelse {
+    const term = args.next() orelse {
         std.debug.print("ERROR: you have to provide a search term\n", .{});
         return;
     };
 
-    try searchDir(".");
+    try searchDir(".", term);
 }
