9c3e1806717aadbb5c6e7982c1589ba10559e8cd
aichingert <aichinger.tobias13@gmail.com>
2024-07-13 12:17:25 +0200

    feat: added simple fetch request

--- src/main.zig
+++ src/main.zig
@@ -1,71 +1,29 @@
 const std = @import("std");
 
-const allocator = std.heap.page_allocator;
-
-fn containsTerm(line: []const u8, term: []const u8) ?usize {
-    var loc: usize = 0;
-    var end: usize = term.len;
-
-    while (end + 1 < line.len) {
-        if (std.mem.eql(u8, line[loc..end], term)) {
-            return loc;
-        }
-
-        loc += 1;
-        end += 1;
-    }
-
-    return null;
-}
-
-fn searchDir(ident: []const u8, term: []const u8) !void {
-    var dir = try std.fs.cwd().openIterableDir(ident, .{});
-    defer dir.close();
-
-    var iter = dir.iterate();
-
-    while (try iter.next()) |entry| {
-        const path = try concatDir(ident, entry.name);
-
-        if (entry.kind == .directory) {
-            try searchDir(path, term);
-            continue;
-        }
-        var file = try std.fs.cwd().openFile(path, .{ .mode = std.fs.File.OpenMode.read_only });
-        defer file.close();
-
-        var buf_reader = std.io.bufferedReader(file.reader());
-        var in_stream = buf_reader.reader();
-        var buf: [1024]u8 = undefined;
-
-        while (in_stream.readUntilDelimiterOrEof(&buf, '\n') catch {
-            continue;
-        }) |line| {
-            if (containsTerm(line, term)) |loc| {
-                std.debug.print("Dir: {s}\n", .{ident});
-                std.debug.print("   - file: {s} | {any}\n", .{ entry.name, entry.kind });
-                std.debug.print("       - {d}\n", .{loc});
-            }
-        }
-    }
-}
-
-fn concatDir(f: []const u8, s: []const u8) ![]const u8 {
-    var res = try allocator.alloc(u8, f.len + s.len + 1);
-    std.mem.copy(u8, res[0..], f);
-    res[f.len] = '/';
-    std.mem.copy(u8, res[f.len + 1 ..], s);
-    return res;
-}
-
 pub fn main() !void {
-    var args = std.process.args();
-    _ = args.skip();
+    // Create a general purpose allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    const term = args.next() orelse {
-        std.debug.print("ERROR: you have to provide a search term\n", .{});
-        return;
-    };
+    // Create a HTTP client
+    var client = std.http.Client{ .allocator = gpa.allocator() };
+    defer client.deinit();
 
-    try searchDir(".", term);
+    // Allocate a buffer for server headers
+    var buf: [4096]u8 = undefined;
+
+    // Start the HTTP request
+    const uri = try std.Uri.parse("https://www.google.com?q=zig");
+    var req = try client.open(.GET, uri, .{ .server_header_buffer = &buf });
+    defer req.deinit();
+
+    // Send the HTTP request headers
+    try req.send();
+    // Finish the body of a request
+    try req.finish();
+
+    // Waits for a response from the server and parses any headers that are sent
+    try req.wait();
+
+    std.debug.print("status={?}\n", .{req.response});
 }
