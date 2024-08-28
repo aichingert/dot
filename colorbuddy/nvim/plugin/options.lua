103a36b55e40733c21f9d0ce57144ed136c7c7c0
aichingert <aichinger.tobias13@gmail.com>
2024-08-29 00:35:30 +0200

    fix: treesitter highlighting

---
+++ colorbuddy/nvim/plugin/options.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+opt.inccommand = "split"
+
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.formatoptions:remove "o"
