d78901ba523a33366966a3db74b9e412c2d87d85
aichingert <aichinger.tobias13@gmail.com>
2024-10-03 09:25:57 +0200

    feat(gruber-darker): added new theme

---
+++ gruber-darker/nvim/plugin/options.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+opt.inccommand = "split"
+opt.clipboard = "unnamedplus"
+
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.termguicolors = true
+
+opt.formatoptions:remove "o"
